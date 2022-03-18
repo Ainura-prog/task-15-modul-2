4e97b828e12892ae2f89543671cc4a3bc13d4fbe
Ainura-prog <ainura.suerkulova.ss@gmail.com>
2022-03-18 23:08:01 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const products = [{
+        id: 1,
+        title: "Ноутбук Sony Vaio VPC-YB2L1R AMD Fusion E-350",
+        category: "ноутбуки",
+        price: 19950,
+        img: "https://www.kivano.kg/images/product/68352/full/1622355077_67139000.png",
+        desc: "Процессор: E-350 Частота процессора: 1600 МГц Объем жесткого диска: 320 ГБ Диагональ экрана: 11.6, Видеокарта: ATI Radeon HD 6310M Вес: 1.46 кг Оптический привод: DVD нет Bluetooth: есть Wi-Fi: есть",
+    },
+    {
+        id: 2,
+        title: "Xiaomi Mi 11 Lite",
+        category: "смартфоны",
+        price: 28700,
+        img: "https://www.kivano.kg/images/product/92416/full/1635945551_75038600.jpg",
+        desc: "экран: 6.55 (2400x1080) 90 Гц, оперативная память: 8 ГБ, память: 128 ГБ, слот для карты памяти, 3 камеры: 64 МП, 8 МП, 5 МП, аккумулятор: 4250 мА·ч, процессор: Qualcomm Snapdragon 780G, SIM-карты: 2 (nano SIM), операционная система: Android 11, беспроводные интерфейсы: NFC, Wi-Fi, Bluetooth 5.1, интернет: 5G, 4G LTE, вес: 159 г",
+    },
+];
+
+const productsInner = document.getElementById("products");
+window.onload = function() {
+    let items = products.map(function(item) {
+        return `<div class="product">
+                <img src="${item.img}" alt="">
+                <div class="product-info">
+                    <div class="product-info-title">${item.title}</div>
+                    <div class="product-info-price">${item.price}</div>
+                </div>
+                <div class="product-desc">${item.desc}</div>
+            </div>`;
+    });
+    items = items.join("");
+    document.getElementById("products").innerHTML += items;
+};
